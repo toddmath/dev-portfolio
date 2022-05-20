a6788ac2e57d332e01bb2c7201cffd69c84df9fb
toddmath <tmatheson11186@gmail.com>
2022-05-20 12:18:34 -0700

    inline solo components into route endpoints

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -1,5 +1,129 @@
-<script lang="ts">
-	import About from '$lib/About.svelte';
+<script context="module">
+	export const prerender = true;
 </script>
 
-<About />
+<script lang="ts">
+	import { fade, fly } from 'svelte/transition';
+	import { cubicIn, cubicOut } from 'svelte/easing';
+	import { CodeIcon } from 'svelte-feather-icons';
+
+	import Section from '$lib/Section.svelte';
+	import { container } from '$lib/action/container';
+
+	let technologies = [
+		'Javascript (ES6+)',
+		'Typescript',
+		'Advanced CSS & Sass',
+		'HTML5',
+		'React',
+		'Redux',
+		'Node.js',
+		'Express',
+		'GraphQL',
+		'Rust',
+		'Lua',
+		'Go'
+	];
+</script>
+
+<svelte:head>
+	<title>About - Todd Matheson</title>
+</svelte:head>
+
+<Section title="About Me" id="about">
+	<div
+		use:container={{
+			xsmall: 0,
+			small: 200,
+			'image-full': 400,
+			large: 500
+		}}
+		in:fade={{ duration: 300, easing: cubicIn }}
+		out:fade={{ duration: 200, easing: cubicOut }}
+		class="transition-all card card-normal lg:card-side bg-base-100 mx-auto justify-center max-w-2xl lg:max-w-6xl skew-x-0 lg:-skew-x-6 hover:skew-x-0 border-4 border-base-content shadow-base-content"
+	>
+		<figure
+			in:fly={{ duration: 300, x: -150, delay: 250, easing: cubicIn }}
+			out:fade={{ duration: 200, easing: cubicOut }}
+			class="transition-all flex-shrink flex-grow basis-20 md:basis-1/3 skew-x-0 translate-x-0 lg:skew-x-6 lg:-translate-x-6"
+		>
+			<!-- svelte-ignore a11y-img-redundant-alt -->
+			<img
+				src="profile.jpeg"
+				alt="Todd's profile picture"
+				class="block w-auto object-cover h-auto md:w-full md:h-full border-base-content border-b-4 lg:border-r-4"
+			/>
+		</figure>
+
+		<div
+			in:fly={{ duration: 300, y: -200, delay: 250, easing: cubicIn }}
+			out:fade={{ duration: 200, easing: cubicOut }}
+			class="transition-all card-body prose mx-auto gap-y-0 lg:pl-0 skew-x-0 lg:skew-x-6 translate-x-0 lg:-translate-x-6 border-base-content"
+		>
+			<h2 in:fly={{ duration: 300, x: 150, delay: 550, easing: cubicIn }} class="card-title">
+				Hi, I'm Todd Matheson.
+			</h2>
+			<p>
+				I'm a passionate, <a
+					class="link link-primary"
+					href="https://www.geeksforgeeks.org/what-is-full-stack-development/"
+					>full stack web developer</a
+				>
+				based in
+				<a class="link link-primary" href="https://goo.gl/maps/BXFweUd8MrrYuSAZ7"
+					>Silicon Valley, California</a
+				>. I love creating software that spands across the internet. I develop modern websites and
+				web apps that provide aesthetic, easy to navigate user interfaces written with the most
+				modern tooling and frameworks.
+			</p>
+
+			<div class="card-actions flex-col">
+				<p class="prose-p text-center self-center mb-3">
+					Here are a few technologies I've been working with recently:
+				</p>
+
+				<ul class="flex flex-wrap justify-center gap-2 md:gap-3 prose-ul px-0">
+					{#each technologies as tech, i}
+						<li
+							in:fly={{ duration: 200, x: 100, delay: i * 125 + 200, easing: cubicOut }}
+							class="list-none font-medium w-max h-4 gap-2 flex badge badge-sm md:badge-md py-2 md:py-2 m-0"
+						>
+							<span class="align-center gap-2">
+								<CodeIcon size="18" strokeWidth={2.5} />
+							</span>
+							{tech}
+						</li>
+					{/each}
+				</ul>
+			</div>
+		</div>
+	</div>
+</Section>
+
+<style>
+	.card {
+		box-shadow: 4px 4px var(--tw-shadow-color, #000000);
+	}
+
+	:global(.card.large img) {
+		object-position: center 18%;
+		aspect-ratio: 18 / 9;
+	}
+
+	.card:hover figure,
+	.card:hover .card-body {
+		--tw-skew-x: 0deg;
+	}
+
+	.card-title {
+		font-size: var(--step-3);
+	}
+
+	.card-body {
+		padding: var(--space-m);
+	}
+
+	ul {
+		font-size: var(--step--1);
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,128 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
 <script lang="ts">
-	import Hero from '$lib/Hero.svelte';
+	import { spring } from 'svelte/motion';
+	import { fade, fly } from 'svelte/transition';
+	import { cubicIn, cubicOut } from 'svelte/easing';
+	import { ArrowRightIcon } from 'svelte-feather-icons';
+	import Button from '$lib/Button.svelte';
+
+	let scroll = 0;
+	let width = 0;
+	let height = 0;
+
+	const mouse = spring(
+		{ x: 0, y: 0, bx: 50, by: 50 },
+		{
+			stiffness: 0.06,
+			damping: 0.4
+		}
+	);
+
+	const onMouseMove = ({ clientX, clientY }: MouseEvent) => {
+		mouse.set({
+			x: Math.trunc(clientX),
+			y: Math.trunc(clientY),
+			bx: (clientX - width) / 150 + 60,
+			by: (clientY - height) / 150 + 60
+		});
+	};
+
+	const onMouseLeave = (_: MouseEvent) => {
+		mouse.set({ x: 0, y: 0, bx: 50, by: 50 });
+	};
 </script>
 
 <svelte:head>
 	<title>Todd's Dev Portfolio</title>
 </svelte:head>
 
-<Hero />
+<svelte:window bind:scrollY={scroll} bind:innerWidth={width} />
+
+<svelte:body on:mousemove={onMouseMove} on:mouseleave={onMouseLeave} />
+
+<section
+	bind:clientHeight={height}
+	transition:fade
+	class="transition-all hero h-full text-secondary relative px-4 cover bg-no-repeat bg-center"
+>
+	<div
+		class="hero-overlay bg-gradient-to-br from-primary to-secondary opacity-50 mix-blend-darken"
+	/>
+	<header
+		in:fly={{ duration: 400, x: -300, easing: cubicIn }}
+		out:fly={{ duration: 400, x: 300, easing: cubicOut }}
+		class="will-change-transform hero-content p-0 text-center card bg-base-100 text-base-content origin-center rounded-box border-4 border-base-content shadow-base-content"
+		style:transform={$mouse.x === 0 && $mouse.y === 0
+			? 'translate3d(0, 0, 0)'
+			: `translate3d(${$mouse.x / 80}px, ${$mouse.y / 80}px, 0)`}
+	>
+		<div
+			in:fly={{ duration: 400, x: -100, delay: 225, easing: cubicOut }}
+			out:fly={{ duration: 400, x: 100, easing: cubicOut }}
+			class="card-body rounded-box origin-center"
+			style:transform={$mouse.x === 0 && $mouse.y === 0
+				? 'translate3d(0, 0, 0)'
+				: `translate3d(${$mouse.x / 160}px, ${$mouse.y / 160}px, 0)`}
+		>
+			<h1 class="card-title flex-col font-semibold">
+				Hi, I'm Todd Matheson.<span class="font-bold italic"
+					>I build beautiful & modern websites and web-apps.</span
+				>
+			</h1>
+
+			<p
+				in:fly={{ duration: 250, x: -125, delay: 275, easing: cubicOut }}
+				out:fly={{ duration: 250, x: 125, easing: cubicOut }}
+				class="leading-8 max-w-prose"
+			>
+				I'm a front end web developer based in San Mateo, CA specializing in building exceptional,
+				high-quality websites & web apps.
+			</p>
+
+			<div
+				in:fly={{ duration: 350, x: -150, delay: 350, easing: cubicOut }}
+				out:fade={{ duration: 200, easing: cubicOut }}
+				class="card-actions justify-center"
+			>
+				<Button
+					tag="a"
+					prefetch
+					href="/projects"
+					size="xl"
+					type="neubrutal"
+					class="transition px-10 font-extrabold gap-2 bg-gradient-to-r from-primary to-secondary box-border hover:border-primary-content"
+				>
+					Checkout My Projects<ArrowRightIcon strokeWidth={3} /></Button
+				>
+			</div>
+		</div>
+	</header>
+</section>
+
+<style>
+	section {
+		background-image: url('/nathan-watson-unsplash_3000x2000_36.avif');
+	}
+
+	header {
+		box-shadow: 4px 4px var(--tw-shadow-color, #000000);
+	}
+
+	.card-body {
+		padding-inline: var(--space-l);
+		padding-block: var(--space-xl);
+		gap: var(--space-m);
+	}
+
+	h1 {
+		font-size: var(--step-4);
+		gap: var(--space-s);
+	}
+
+	h1 > span {
+		font-size: var(--step-3);
+	}
+</style>
